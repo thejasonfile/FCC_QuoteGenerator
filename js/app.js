26a0a860110634debc789e7d7416b04fc566a602
Jason Arnold <jason@thejasonfile.com>
2015-09-01 21:35:14 -0400

    finished basic page

---
+++ js/app.js
@@ -0,0 +1,80 @@
+var quotes = [
+	["Science is a way of thinking much more than it is a body of knowledge.","Carl Sagan"],
+	["Imagination will often carry us to worlds that never were. But without it we go nowhere.", "Carl Sagan"],
+	["Somewhere, something incredible is waiting to be known.", "Carl Sagan"],
+	["We have also arranged things so that almost no one understands science and technology. This is a prescription for disaster. We might get away with it for a while, but sooner or later this combustible mixture of ignorance and power is going to blow up in our faces.", "Carl Sagan"],
+	["For small creatures such as we the vastness is bearable only through love.","Carl Sagan"],
+	["Skeptical scrutiny is the means, in both science and religion, by which deep thoughts can be winnowed from deep nonsense.","Carl Sagan"],
+	["All of the books in the world contain no more information than is broadcast as video in a single large American city in a single year. Not all bits have equal value.","Carl Sagan"],
+	["Our species needs, and deserves, a citizenry with minds wide awake and a basic understanding of how the world works.","Carl Sagan"],
+	["I am often amazed at how much more capability and enthusiasm for science there is among elementary school youngsters than among college students.","Carl Sagan"],
+	["A celibate clergy is an especially good idea, because it tends to suppress any hereditary propensity toward fanaticism.","Carl Sagan"],
+	["If we long to believe that the stars rise and set for us, that we are the reason there is a Universe, does science do us a disservice in deflating our conceits?","Carl Sagan"],
+	["We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology.","Carl Sagan"],
+	["Extinction is the rule. Survival is the exception.","Carl Sagan"],
+	["Personally, I would be delighted if there were a life after death, especially if it permitted me to continue to learn about this world and others, if it gave me a chance to discover how history turns out.","Carl Sagan"],
+	["The brain is like a muscle. When it is in use we feel very good. Understanding is joyous.","Carl Sagan"],
+	["It is far better to grasp the universe as it really is than to persist in delusion, however satisfying and reassuring.","Carl Sagan"],
+	["We've arranged a civilization in which most crucial elements profoundly depend on science and technology.","Carl Sagan"],
+	["I can find in my undergraduate classes, bright students who do not know that the stars rise and set at night, or even that the Sun is a star.","Carl Sagan"],
+	["Who are we? We find that we live on an insignificant planet of a humdrum star lost in a galaxy tucked away in some forgotten corner of a universe in which there are far more galaxies than people.","Carl Sagan"],
+	["If you wish to make an apple pie from scratch, you must first invent the universe.","Carl Sagan"],
+	["For me, it is far better to grasp the Universe as it really is than to persist in delusion, however satisfying and reassuring.","Carl Sagan"],
+	["Absence of evidence is not evidence of absence.","Carl Sagan"],
+	["When you make the finding yourself - even if you're the last person on Earth to see the light - you'll never forget it.","Carl Sagan"],
+	["The universe is not required to be in perfect harmony with human ambition.","Carl Sagan"],
+	["The universe seems neither benign nor hostile, merely indifferent.","Carl Sagan"],
+	["But the fact that some geniuses were laughed at does not imply that all who are laughed at are geniuses. They laughed at Columbus, they laughed at Fulton, they laughed at the Wright Brothers. But they also laughed at Bozo the Clown.","Carl Sagan"],
+	["Humans aren't as good as we should be in our capacity to empathize with feelings and thoughts of others, be they humans or other animals on Earth. So maybe part of our formal education should be training in empathy. Imagine how different the world would be if, in fact, that were 'reading, writing, arithmetic, empathy.'","Neil deGrasse Tyson"],
+	["Let me tell you something about full moons: kids don't care about full moons. They'll play in a full moon, no worries at all. They only get scared of magic or werewolves from stupid adults and their stupid adult stories.","Neil deGrasse Tyson"],
+	["Once you have an innovation culture, even those who are not scientists or engineers - poets, actors, journalists - they, as communities, embrace the meaning of what it is to be scientifically literate. They embrace the concept of an innovation culture. They vote in ways that promote it. They don't fight science and they don't fight technology.","Neil deGrasse Tyson"],
+	["No one is dumb who is curious. The people who don't ask questions remain clueless throughout their lives.","Neil deGrasse Tyson"],
+	["I have a personal philosophy in life: If somebody else can do something that I'm doing, they should do it. And what I want to do is find things that would represent a unique contribution to the world - the contribution that only I, and my portfolio of talents, can make happen. Those are my priorities in life.","Neil deGrasse Tyson"],
+	["I think that intelligence is such a narrow branch of the tree of life - this branch of primates we call humans. No other animal, by our definition, can be considered intelligent. So intelligence can't be all that important for survival, because there are so many animals that don't have what we call intelligence, and they're surviving just fine.","Neil deGrasse Tyson"],
+	["I've spent quality time in the aerospace community, with my service on two presidential commissions, but at heart, I'm an academic. Being an academic means I don't wield power over person, place or thing. I don't command armies; I don't lead labor unions. All I have is the power of thought.","Neil deGrasse Tyson"],
+	["Kids should be allowed to break stuff more often. That's a consequence of exploration. Exploration is what you do when you don't know what you're doing.","Neil deGrasse Tyson"],
+	["Perhaps we've never been visited by aliens because they have looked upon Earth and decided there's no sign of intelligent life.","Neil deGrasse Tyson"],
+	["Dreams about the future are always filled with gadgets.","Neil deGrasse Tyson"],
+	["Space exploration is a force of nature unto itself that no other force in society can rival.","Neil deGrasse Tyson"],
+	["I try to show the public that chemistry, biology, physics, astrophysics is life. It is not some separate subject that you have to be pulled into a corner to be taught about.","Neil deGrasse Tyson"],
+	["Even with all our technology and the inventions that make modern life so much easier than it once was, it takes just one big natural disaster to wipe all that away and remind us that, here on Earth, we're still at the mercy of nature.","Neil deGrasse Tyson"],
+	["If your ego starts out, 'I am important, I am big, I am special,' you're in for some disappointments when you look around at what we've discovered about the universe. No, you're not big. No, you're not. You're small in time and in space. And you have this frail vessel called the human body that's limited on Earth.","Neil deGrasse Tyson"],
+	["We are part of this universe; we are in this universe, but perhaps more important than both of those facts, is that the universe is in us.","Neil deGrasse Tyson"],
+	["What you need, above all else, is a love for your subject, whatever it is. You've got to be so deeply in love with your subject that when curve balls are thrown, when hurdles are put in place, you've got the energy to overcome them.","Neil deGrasse Tyson"],
+	["Any time scientists disagree, it's because we have insufficient data. Then we can agree on what kind of data to get; we get the data; and the data solves the problem. Either I'm right, or you're right, or we're both wrong. And we move on. That kind of conflict resolution does not exist in politics or religion.","Neil deGrasse Tyson"],
+	["Passion is what gets you through the hardest times that might otherwise make strong men weak, or make you give up.","Neil deGrasse Tyson"],
+	["If the only time you think of me as a scientist is during Black History Month, then I must not be doing my job as a scientist.","Neil deGrasse Tyson"],
+	["Every account of a higher power that I've seen described, of all religions that I've seen, include many statements with regard to the benevolence of that power. When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.","Neil deGrasse Tyson"],
+	["The problem is not scientifically illiterate kids; it is scientifically illiterate adults. Kids are born curious about the natural world. They are always turning over rocks, jumping with two feet into mud puddles and playing with the tablecloth and fine china.","Neil deGrasse Tyson"],
+	["Those who see the cosmic perspective as a depressing outlook, they really need to reassess how they think about the world. Because when I look up in the universe, I know I'm small but I'm also big. I'm big because I'm connected to the universe, and the universe is connected to me.","Neil deGrasse Tyson"],
+	["I said that if an alien came to visit, I'd be embarrassed to tell them that we fight wars to pull fossil fuels out of the ground to run our transportation. They'd be like, 'What?'","Neil deGrasse Tyson"],
+	["In any city with lots of skyscrapers, lots of skyline, the moon seems bigger than it is. It's called the moon illusion.","Neil deGrasse Tyson"],
+	["I can't gather around and talk about how much everybody in the room doesn't believe in God. I just don't - I don't have the energy for that, and so I... Agnostic separates me from the conduct of atheists whether or not there is strong overlap between the two categories, and at the end of the day I'd rather not be any category at all.","Neil deGrasse Tyson"],
+	["Most of what Einstein said and did has no direct impact on what anybody reads in the Bible. Special relativity, his work in quantum mechanics, nobody even knows or cares. Where Einstein really affects the Bible is the fact that general relativity is the organizing principle for the Big Bang.","Neil deGrasse Tyson"],
+	["Not enough books focus on how a culture responds to radically new ideas or discovery. Especially in the biography genre, they tend to focus on all the sordid details in the life of the person who made the discovery. I find this path to be voyeuristic but not enlightening.","Neil deGrasse Tyson"],
+	["The partisanship surrounding space exploration and the retrenching of U.S. space policy are part of a more general trend: the decline of science in the United States. As its interest in science wanes, the country loses ground to the rest of the industrialized world in every measure of technological proficiency.","Neil deGrasse Tyson"],
+	["It was unthinkable not long ago that a biologist or paleontologist would be at the same conference as an astrophysicist. Now we have accumulated so much data in each of these branches of science as it relates to origins that we have learned that no one discipline can answer questions of origins alone.","Neil deGrasse Tyson"],
+	["Half of my library are old books because I like seeing how people thought about their world at their time. So that I don't get bigheaded about something we just discovered and I can be humble about where we might go next. Because you can see who got stuff right and most of the people who got stuff wrong.","Neil deGrasse Tyson"],
+	["Do you realize that if you fall into a black hole, you will see the entire future of the Universe unfold in front of you in a matter of moments and you will emerge into another space-time created by the singularity of the black hole you just fell into?","Neil deGrasse Tyson"],
+	["In the animal kingdom, one of the keys to survival is to outwit your enemies. And when you're surrounded by carnivores, one of the best strategies is to fade into the background and disappear.","Neil deGrasse Tyson"],
+	["The supermoon is a 16-inch pizza compared with a 15-inch pizza. It's a slightly bigger moon; I ain't using the adjective 'supermoon.'","Neil deGrasse Tyson"],
+	["Space enthusiasts are the most susceptible demographic to delusion that I have ever seen.","Neil deGrasse Tyson"],
+	["I was transformed by picking up a pair of binoculars and looking up, and that's hard to do for a city kid because when you look up you just see buildings - and really, your first thought is to look in people's windows. So to look out of the space - out of living space - and look up to the sky, binoculars go far, literally and figuratively.","Neil deGrasse Tyson"],
+];
+
+function newQuote() {
+	//generate random number
+	var random=Math.floor(Math.random()*(quotes.length - 1));
+	//take index of quotes that matches the random number and add both its [0] and [1] element to the #quote div
+	$("#quote").html("<h3>" + quotes[random][0] + "</h3><br /><hr><h4>" + quotes[random][1] + "</h4>");	
+}
+
+//call newQuote funciton when page loads
+$(document).ready(function() {
+	newQuote();
+});
+
+//call newQuote function when button is clicked
+$(".btn").click(function() {
+	newQuote();
+});
